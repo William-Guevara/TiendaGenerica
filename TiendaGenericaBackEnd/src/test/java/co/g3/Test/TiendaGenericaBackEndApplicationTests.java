2fe35a9ce942919c7973dedc8c69c5bd8598a0c7
William-Guevara <william.guevara@cun.edu.co>
2021-09-22 15:03:01 -0500

    carga project

---
+++ TiendaGenericaBackEnd/src/test/java/co/g3/Test/TiendaGenericaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package co.g3.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaGenericaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
